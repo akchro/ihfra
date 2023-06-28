ddc9b0c97fc54ea7b2234a945324c6d06d309f72
Andy Khau <fakeemail1441@gmail.com>
2023-06-28 03:15:31 -0700

    Finish globe

--- app/page.js
+++ app/page.js
@@ -1,13 +1,28 @@
 import Image from 'next/image'
 import GlobeComponent from "@/assets/globe";
+import {Suspense} from "react";
+import Loading from "@/app/loading";
+import IhfraSplash from '../assets/ihfrasplash.png'
 
 
 export default function Home() {
   return (
     <main id={'root'}>
-        <div className={'h-screen w-screen'}>
+
+        <div className={"w-screen text-center content-center flex justify-center overflow-hidden"}>
+            <div className={"absolute"}>
+                <h1 className={"text-[300px] z-0"}>
+                    IHFRA
+                </h1>
+            </div>
+        </div>
+        
+        <div className={'h-[900px] w-full z-10 absolute top-[50px]'}>
             <GlobeComponent/>
         </div>
+        {/*<div className={'h-[900px]'}>*/}
+        {/*    test*/}
+        {/*</div>*/}
     </main>
   )
 }
--- assets/globe.js
+++ assets/globe.js
@@ -4,7 +4,9 @@ import React, {useRef, useLayoutEffect} from 'react';
 import {Canvas, useFrame, extend, useThree} from "@react-three/fiber";
 import ThreeGlobe from "three-globe";
 import countries from './globe-data-min.json';
+import {Suspense} from "react";
 import {Color, DirectionalLight, PointLight, NoToneMapping} from "three";
+import Loading from "@/app/loading";
 
 extend({ThreeGlobe})
 
@@ -12,7 +14,6 @@ const CameraPosition = () => {
     const { camera } = useThree();
     camera.position.z = 250;
     camera.fov = 50
-
     return null;
 };
 
@@ -20,6 +21,18 @@ const CameraPosition = () => {
 const GlobeObj = () => {
 
     const globeRef = useRef();
+
+
+    // data for arcs
+    const N = 50;
+
+    const arcsData = [...Array(N).keys()].map(() => ({
+        startLat: (Math.random() - 0.5) * 180,
+        startLng: (Math.random() - 0.5) * 360,
+        endLat: (Math.random() - 0.5) * 180,
+        endLng: (Math.random() - 0.5) * 360,
+        color: ['#7b2080', 'white', '#4fa1e1', '#c21bcb'][Math.round(Math.random() * 3)]
+    }));
 
     useFrame( () => {
             if (globeRef.current) {
@@ -34,6 +47,7 @@ const GlobeObj = () => {
         globeMaterial.emissive = new Color(0x220038);
         globeMaterial.emissiveIntensity = 0.1;
         globeMaterial.shininess = 0.7;
+
 
         globeRef.current.showAtmosphere(true)
         globeRef.current.atmosphereColor("#3a228a")
@@ -54,6 +68,14 @@ const GlobeObj = () => {
 
         globeRef.current.rotation.x = .5
         globeRef.current.rotation.z = .3
+
+        globeRef.current.arcsData(arcsData)
+        globeRef.current.arcColor('color')
+        globeRef.current.arcDashLength(0.4)
+        globeRef.current.arcDashGap(4)
+        globeRef.current.arcDashInitialGap(() => Math.random() * 5)
+        globeRef.current.arcDashAnimateTime(2000)
+
     })
 
     return (
@@ -69,20 +91,24 @@ const Globe = () => {
     return (
         <Canvas
             gl={{ antialias: true, toneMapping: NoToneMapping }}
+            resize={{scroll: false}}
         >
-            <ambientLight intensity={0.01} color={0xbbbbbb} />
-            <fog attach={'fog'} color={0x535ef3} near={1} far={2000}/>
-            <directionalLight color={0xffffff} intensity={0.5} position={[-800, 1500, 300]}/>
-            <directionalLight color={0x7982f6} intensity={0.8} position={[-300, 400, 50]}/>
-            <spotLight color={'802959'} intensity={0.8} position={[50, 500, -40]} angle={0.5}/>
-            <directionalLight color={'#bff7ff'} intensity={3} position={[-300, 400, -300]}/>
-            <directionalLight color={'#bff7ff'} intensity={2} position={[-300, 400, -150]}/>
-            <directionalLight color={'#bff7ff'} intensity={50} position={[-250, 250, -450]}/>
-            <directionalLight color={'#bff7ff'} intensity={50} position={[-270, 0, -470]}/>
-            <directionalLight color={'#bff7ff'} intensity={50} position={[0, 250, -500]}/>
-            <pointLight color={0x8566cc} intensity={0.8} position={[-200, 500, 450]}/>
-            <CameraPosition/>
-            <GlobeObj/>
+
+            <Suspense fallback={<Loading/>}>
+                <ambientLight intensity={0.01} color={0xbbbbbb}/>
+                <fog attach={'fog'} color={0x535ef3} near={1} far={2000}/>
+                <directionalLight color={0xffffff} intensity={0.5} position={[-800, 1500, 300]}/>
+                <directionalLight color={0x7982f6} intensity={0.8} position={[-300, 400, 50]}/>
+                <spotLight color={'802959'} intensity={0.8} position={[50, 500, -40]} angle={0.5}/>
+                <directionalLight color={'#bff7ff'} intensity={3} position={[-300, 400, -300]}/>
+                <directionalLight color={'#bff7ff'} intensity={2} position={[-300, 400, -150]}/>
+                <directionalLight color={'#bff7ff'} intensity={50} position={[-250, 250, -450]}/>
+                <directionalLight color={'#bff7ff'} intensity={50} position={[-270, 0, -470]}/>
+                <directionalLight color={'#bff7ff'} intensity={50} position={[0, 250, -500]}/>
+                <pointLight color={0x8566cc} intensity={0.8} position={[-200, 500, 450]}/>
+                <CameraPosition/>
+                <GlobeObj/>
+            </Suspense>
 
         </Canvas>
     );
