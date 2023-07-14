8d6b1aa6ac83722f3613ac0419b1dfa0861a7962
Andy Khau <fakeemail1441@gmail.com>
2023-07-14 12:50:12 -0700

    Change IHFRA to HELLO

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import GlobeComponent from "@/assets/globe";
+import Card from "@/Components/card";
 import {Suspense} from "react";
 import Loading from "@/app/loading";
 import IhfraSplash from '../assets/ihfrasplash.png'
@@ -9,20 +10,24 @@ export default function Home() {
   return (
     <main id={'root'}>
 
-        <div className={"w-screen text-center content-center flex justify-center overflow-hidden"}>
-            <div className={"absolute"}>
-                <h1 className={"text-[300px] z-0"}>
-                    IHFRA
-                </h1>
+        <div className={'bg-black h-[900px] rounded-b-[200px]'}>
+            <div className={"w-screen text-center content-center flex justify-center overflow-hidden"}>
+                <div className={"absolute"}>
+                    <h1 className={"text-[300px] z-0 text-white"}>
+                        HELLO
+                    </h1>
+                </div>
+            </div>
+
+            <div className={'h-full w-full z-10 absolute top-[50px]'}>
+                <GlobeComponent/>
             </div>
         </div>
-        
-        <div className={'h-[900px] w-full z-10 absolute top-[50px]'}>
-            <GlobeComponent/>
+        {/*<div className={'h-80 bg-gradient-to-b from-black via-gray-500 to-white'}></div>*/}
+        <div className={'h-[900px] pt-36 px-36'}>
+            <Card stat={"150+"} description={"members"}/>
         </div>
-        {/*<div className={'h-[900px]'}>*/}
-        {/*    test*/}
-        {/*</div>*/}
+
     </main>
   )
 }
